2681965f4c9b19341617d21fd8d7dbd181448e27
Fernando Lucas <103616122+FernandoLucasDev@users.noreply.github.com>
2025-05-17 13:05:38 -0300

    Implement job to catch news

--- main.go
+++ main.go
@@ -4,14 +4,29 @@ import (
 	"api-echo/db"
 	"api-echo/routes"
 	"api-echo/services"
+	"log"
 
 	"github.com/labstack/echo/v4"
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
+
 	e := echo.New()
+	c := cron.New()
+
 	db.Connect()
 	routes.Init(e)
-	services.FetchAndStoreNews()
 	e.Logger.Fatal(e.Start(":8080"))
+
+	_, err := c.AddFunc("0 8,14,20 * * *", func() {
+		log.Println("🔁 Executando job de notícias...")
+		services.FetchAndStoreNews()
+	})
+	if err != nil {
+		log.Fatal("Erro ao agendar o job:", err)
+	}
+
+	c.Start()
+	
 }
